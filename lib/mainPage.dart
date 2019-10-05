efb1f9ad97913869025fd8006f0e276aba6e941c
D Gayathri <d.gaya32611@gmail.com>
2019-10-05 13:11:24 +0530

    partial module 1

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -4,6 +4,8 @@ class mainPage extends StatefulWidget {
   _mainPageState createState() => _mainPageState();
 }
 class _mainPageState extends State<mainPage> {
+  get wrap => null;
+
   @override
   Widget build(BuildContext context) {
     return
@@ -18,30 +20,12 @@ class _mainPageState extends State<mainPage> {
               fit: BoxFit.cover,
             ),
           ),
-              child: ListView.builder(itemBuilder: null),
-              scrollDirection: Axis.vertical,
-            children :<Widget>[
-              Center(
-                child: Container(
-                  child: Card(
-                    color: Color(0xc4ffffff),
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)
-                    ),
-                    child: InkWell(
-                      splashColor: Colors.blue.withAlpha(30),
-                      onTap: () {
-                        print('Card tapped.');
-                      },
-                      child: Container(
-                        width: 430,
-                        height: 150,
-                        child: Text('A card that can be tapped'),
-                      ),
-                    ),
-                    elevation: 14,
-                  ),
-                ),
-              ),
+              child: ListView(
+                scrollDirection: Axis.vertical,
+
+              children :<Widget>[
+
+
               Center(
                 child: Container(
                   child: Card(
@@ -147,15 +131,61 @@ class _mainPageState extends State<mainPage> {
                   ),
                 ),
               ),
+                Center(
+                  child: Container(
+                    child: Card(
+                      color: Color(0xc4ffffff),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)
+                      ),
+                      child: InkWell(
+                        splashColor: Colors.blue.withAlpha(30),
+                        onTap: () {
+                          print('Card tapped.');
+                        },
+                        child: Container(
+                          width: 430,
+                          height: 150,
+                          child: Text('A card that can be tapped'),
+                        ),
+                      ),
+                      elevation: 14,
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    child: Card(
+                      color: Color(0xc4ffffff),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)
+                      ),
+                      child: InkWell(
+                        splashColor: Colors.blue.withAlpha(30),
+                        onTap: () {
+                          print('Card tapped.');
+                        },
+                        child: Container(
+                          width: 430,
+                          height: 150,
+                          child: Text('A card that can be tapped'),
+                        ),
+                      ),
+                      elevation: 14,
+                    ),
+                  ),
+                ),
                   TextFormField(
                   decoration: InputDecoration(
     labelText: 'Enter your reminder', border: OutlineInputBorder(borderRadius: BorderRadius.circular(0.0),),
 fillColor: Color(0xffffffff), filled: true
                     )
     ),
-            ],
-            )
-              )
-      );
+    ],
+                ),
+
+
+              ),
+            );
+
+
   }
 }
